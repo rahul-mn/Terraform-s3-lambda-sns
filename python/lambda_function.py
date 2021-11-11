db1be7ce18b5285657f994f7bdf71b89bb34978a
Rahul Meena <meenarahul67@gmail.com>
2021-11-11 21:57:51 +0530

    added doc_strings for every action

--- python/lambda_function.py
+++ python/lambda_function.py
@@ -3,19 +3,25 @@ import boto3
 
 print('Loading function')
 
+#defining the client as S3
 s3 = boto3.client('s3')
 
 
 def lambda_handler(event, context):
     #print("Received event: " + json.dumps(event, indent=2))
-
     # Get the object from the event and show its content type
+    
+    #Grab the bucket name from event message and store in var bucket
     bucket = event['Records'][0]['s3']['bucket']['name']
+    
+    #Grab the Object key(filename with extension) from the event msg and convert the ASCII into utf-8
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     try:
+        #Store the metadata of var(key) on var(response)
         response = s3.get_object(Bucket=bucket, Key=key)
+        #From the metadata, print the ContentType of the object 
         print("CONTENT TYPE: " + response['ContentType'])
-        #return response['ContentType']
+        #return the output with object name 
         return "object {} have been uploaded successfully.".format(key)
     except Exception as e:
         print(e)
